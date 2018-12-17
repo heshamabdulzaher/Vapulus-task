0a21115c465bc8e34225cd1dd69d6f1358bf101e
hesham <heshamabdulzaher@gmail.com>
2018-12-18 01:15:37 +0200

    Get recent & all contacts from db and render it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { routingComponents, AppRoutingModule } from "./app-routing.module";
 
-import { AppComponent } from './app.component';
-import { ContactListComponent } from './contact-list/contact-list.component';
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ContactListComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, routingComponents],
+  imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: "app-contact-list",
@@ -6,7 +7,48 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./contact-list.component.css"]
 })
 export class ContactListComponent implements OnInit {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  allContacts;
+  recentContacts;
+
+  ngOnInit() {
+    this.getRecentContacts();
+    this.getAllContacts();
+  }
+  // Get Recent contacts
+  getRecentContacts() {
+    this.http.get("http://localhost:3000/recent-contact").subscribe(res => {
+      this.recentContacts = res;
+    });
+  }
+  // Get All contacts
+  getAllContacts() {
+    this.http.get("http://localhost:3000/contacts").subscribe((data: any) => {
+      // we want to group the items by the first letter
+      let formatedDataAsObject = data.reduce((acc, current) => {
+        let firstLetter = current.firstName.charAt().toLowerCase();
+
+        // If accumulator have this letter
+        if (acc[firstLetter]) {
+          acc[firstLetter]["items"].push(current);
+        } else {
+          // If don't
+          acc[firstLetter] = {
+            letter: firstLetter,
+            items: [current]
+          };
+        }
+        return acc;
+      }, {});
+
+      // Convert the data to array so we can easily loop over it
+      let formatedDataAsArray = [];
+      for (let key in formatedDataAsObject) {
+        formatedDataAsArray.push(formatedDataAsObject[key]);
+      }
+      this.allContacts = formatedDataAsArray;
+    });
+  }
 }
