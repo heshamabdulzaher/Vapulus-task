3a4894a93a34a21dc3fc99a4bf4b3de61808b63a
hesham <heshamabdulzaher@gmail.com>
2018-12-18 21:01:41 +0200

    Add upload photo input to the contact form

--- src/app/add-new-contact/add-new-contact.component.ts
+++ src/app/add-new-contact/add-new-contact.component.ts
@@ -10,9 +10,12 @@ import { Router } from "@angular/router";
 export class AddNewContactComponent implements OnInit {
   constructor(private http: HttpClient, private router: Router) {}
 
+  photoUploadedSrc;
+
   ngOnInit() {}
 
   handlerFunction(contactForm) {
+    console.log(contactForm.form.value);
     if (contactForm.form.valid) {
       // Done
       this.http
@@ -21,6 +24,18 @@ export class AddNewContactComponent implements OnInit {
           contactForm.form.reset();
           this.router.navigate(["/"]);
         });
+    }
+  }
+
+  readURL(e, avatar) {
+    if (e.target.files && e.target.files[0]) {
+      var reader = new FileReader();
+      reader.onload = (res: any) => {
+        avatar.style.backgroundImage = `url(' ${res.target.result} ')`;
+        avatar.style.backgroundSize = "cover";
+        avatar.style.border = "0";
+      };
+      reader.readAsDataURL(e.target.files[0]);
     }
   }
 }
