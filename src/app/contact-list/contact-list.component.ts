4caee2d922958751130e304708fcbc1824283b21
hesham <heshamabdulzaher@gmail.com>
2018-12-18 13:01:04 +0200

    Search functionality :wink:

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -11,7 +11,11 @@ export class ContactListComponent implements OnInit {
 
   alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
   allContacts;
+  formatedContacts;
   recentContacts;
+  serachMode: boolean = false;
+  searchQueryWord: string = "";
+  searchResultItems = [];
 
   ngOnInit() {
     this.getRecentContacts();
@@ -26,6 +30,7 @@ export class ContactListComponent implements OnInit {
   // Get All contacts
   getAllContacts() {
     this.http.get("http://localhost:3000/contacts").subscribe((data: any) => {
+      this.allContacts = data;
       // we want to group the items by the first letter
       let formatedDataAsObject = data.reduce((acc, current) => {
         let firstLetter = current.firstName.charAt().toLowerCase();
@@ -48,7 +53,23 @@ export class ContactListComponent implements OnInit {
       for (let key in formatedDataAsObject) {
         formatedDataAsArray.push(formatedDataAsObject[key]);
       }
-      this.allContacts = formatedDataAsArray;
+      this.formatedContacts = formatedDataAsArray;
+    });
+  }
+
+  // Toggle Search mode to show and hidden search results
+  toggleSearchMode() {
+    this.serachMode = !this.serachMode;
+  }
+
+  // Get seach query word
+  getSearchQueryWors(e) {
+    this.searchResultItems = [];
+    // search with query word over all contacts and that contact will match push it in searchResultItems array
+    this.allContacts.forEach(item => {
+      if (!item.firstName.toLowerCase().search(e)) {
+        this.searchResultItems.push(item);
+      }
     });
   }
 }
