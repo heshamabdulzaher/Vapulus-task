504109b26704b21040caf65036d252c7b3536b70
hesham <heshamabdulzaher@gmail.com>
2018-12-18 03:34:28 +0200

    Ui of add contact form using angular material

--- src/app/add-new-contact/add-new-contact.component.ts
+++ src/app/add-new-contact/add-new-contact.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-add-new-contact',
-  templateUrl: './add-new-contact.component.html',
-  styleUrls: ['./add-new-contact.component.css']
+  selector: "app-add-new-contact",
+  templateUrl: "./add-new-contact.component.html",
+  styleUrls: ["./add-new-contact.component.css"]
 })
 export class AddNewContactComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,23 @@ import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { routingComponents, AppRoutingModule } from "./app-routing.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatInputModule } from "@angular/material";
+import { MatButtonModule } from "@angular/material/button";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent, routingComponents],
-  imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
