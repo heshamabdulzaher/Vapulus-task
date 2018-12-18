b124723dc0e38dd29454c1a0f3c1ef662821b6bd
hesham <heshamabdulzaher@gmail.com>
2018-12-18 02:03:41 +0200

    Add new contact go to 2nd screen ( Add contact form )

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { ContactListComponent } from "./contact-list/contact-list.component";
+import { AddNewContactComponent } from "./add-new-contact/add-new-contact.component";
 const routes: Routes = [
   { path: "", redirectTo: "contact-list", pathMatch: "full" },
-  { path: "contact-list", component: ContactListComponent }
+  { path: "contact-list", component: ContactListComponent },
+  { path: "add-new-contact", component: AddNewContactComponent }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
-export const routingComponents = [ContactListComponent];
+export const routingComponents = [ContactListComponent, AddNewContactComponent];
